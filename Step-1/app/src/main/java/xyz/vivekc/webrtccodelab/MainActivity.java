4a845429f144e50bac9d12d2d4f1f5434580c961
vivek.chanddru <vivek.chanddru@zohocorp.com>
2017-07-10 11:05:55 +0530

    Updated to new WebRTC library

--- Step-1/app/src/main/java/xyz/vivekc/webrtccodelab/MainActivity.java
+++ Step-1/app/src/main/java/xyz/vivekc/webrtccodelab/MainActivity.java
@@ -1,31 +1,24 @@
 package xyz.vivekc.webrtccodelab;
 
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import org.webrtc.AudioSource;
 import org.webrtc.AudioTrack;
-import org.webrtc.CameraVideoCapturer;
-import org.webrtc.DataChannel;
-import org.webrtc.IceCandidate;
+import org.webrtc.Camera1Enumerator;
+import org.webrtc.CameraEnumerator;
+import org.webrtc.EglBase;
+import org.webrtc.Logging;
 import org.webrtc.MediaConstraints;
-import org.webrtc.MediaStream;
-import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
-import org.webrtc.RendererCommon;
+import org.webrtc.SurfaceViewRenderer;
 import org.webrtc.VideoCapturer;
-import org.webrtc.VideoCapturerAndroid;
 import org.webrtc.VideoRenderer;
-import org.webrtc.VideoRendererGui;
 import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,88 +35,72 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Now create a VideoCapturer instance. Callback methods are there if you want to do something! Duh!
-        VideoCapturer videoCapturerAndroid = getVideoCapturer(new CameraVideoCapturer.CameraEventsHandler() {
-            @Override
-            public void onCameraError(String s) {
-                Log.d("onCameraError", s);
-            }
-
-            @Override
-            public void onCameraFreezed(String s) {
-                Log.d("onCameraFreezed", s);
-            }
-
-            @Override
-            public void onCameraOpening(int i) {
-                Log.d("onCameraOpening", i + "");
-            }
-
-            @Override
-            public void onFirstFrameAvailable() {
-                Log.d("onFirstFrameAvailable", "Hola");
-            }
-
-            @Override
-            public void onCameraClosed() {
-                Log.d("onCameraClosed", ":(");
-            }
-        });
-
+        VideoCapturer videoCapturerAndroid = createVideoCapturer();
         //Create MediaConstraints - Will be useful for specifying video and audio constraints. More on this later!
         MediaConstraints constraints = new MediaConstraints();
 
         //Create a VideoSource instance
-        VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturerAndroid, constraints);
+        VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturerAndroid);
         VideoTrack localVideoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
 
         //create an AudioSource instance
         AudioSource audioSource = peerConnectionFactory.createAudioSource(constraints);
         AudioTrack localAudioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
 
+        //we will start capturing the video from the camera
+        //width,height and fps
+        videoCapturerAndroid.startCapture(1000, 1000, 30);
 
-        //We use the VideoRendererGui for displaying video from Cam.
-        //Get the surfaceview instance.
-        GLSurfaceView videoView = (GLSurfaceView) findViewById(R.id.gl_surface_view);
+        //create surface renderer, init it and add the renderer to the track
+        SurfaceViewRenderer videoView = (SurfaceViewRenderer) findViewById(R.id.surface_rendeer);
+        videoView.setMirror(true);
 
-        // Then we set that view, and pass a Runnable to run once the surface is ready
-        VideoRendererGui.setView(videoView, new Runnable() {
-            @Override
-            public void run() {
-                Log.d("run", "VideoRendererGUI callback");
-            }
-        });
+        EglBase rootEglBase = EglBase.create();
+        videoView.init(rootEglBase.getEglBaseContext(), null);
 
-        // Now that VideoRendererGui is ready, we can get our VideoRenderer
-        VideoRenderer renderer = null;
-        try {
-            renderer = VideoRendererGui.createGui(0, 0, 100,100, RendererCommon.ScalingType.SCALE_ASPECT_FIT,true);
-            // And finally, with our VideoRenderer ready, we
-            // can add our renderer to the VideoTrack.
-            localVideoTrack.addRenderer(renderer);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        localVideoTrack.addRenderer(new VideoRenderer(videoView));
+
+
     }
 
-    // Cycle through likely device names for the camera and return the first
-    // capturer that works, or crash if none do.// TODO: 16/10/16 don't crash the app. Not good.
-    private VideoCapturer getVideoCapturer(CameraVideoCapturer.CameraEventsHandler eventsHandler) {
-        String[] cameraFacing = { "front", "back" };
-        int[] cameraIndex = { 0, 1 };
-        int[] cameraOrientation = { 0, 90, 180, 270 };
-        for (String facing : cameraFacing) {
-            for (int index : cameraIndex) {
-                for (int orientation : cameraOrientation) {
-                    String name = "Camera " + index + ", Facing " + facing +
-                            ", Orientation " + orientation;
-                    VideoCapturer capturer = VideoCapturerAndroid.create(name,eventsHandler);
-                    if (capturer != null) {
-                        Log.d("Using camera: " ,name);
-                        return capturer;
-                    }
+
+    private VideoCapturer createVideoCapturer() {
+        VideoCapturer videoCapturer;
+        Logging.d(TAG, "Creating capturer using camera1 API.");
+        videoCapturer = createCameraCapturer(new Camera1Enumerator(false));
+
+        return videoCapturer;
+    }
+
+    private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        // First, try to find front facing camera
+        Logging.d(TAG, "Looking for front facing cameras.");
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                Logging.d(TAG, "Creating front facing camera capturer.");
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
                 }
             }
         }
-        throw new RuntimeException("Failed to open capturer");
+
+        // Front facing camera not found, try something else
+        Logging.d(TAG, "Looking for other cameras.");
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                Logging.d(TAG, "Creating other camera capturer.");
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
     }
 }
