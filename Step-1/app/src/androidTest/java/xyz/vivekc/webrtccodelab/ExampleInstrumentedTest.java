914dc616499154dd431878567fa166782ea725a4
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2020-08-24 23:45:07 +0530

    Update ExampleInstrumentedTest.java

--- Step-1/app/src/androidTest/java/xyz/vivekc/webrtccodelab/ExampleInstrumentedTest.java
+++ Step-1/app/src/androidTest/java/xyz/vivekc/webrtccodelab/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.webrtc", appContext.getPackageName());
+        assertEquals("xyz.vivekc.webrtccodelab", appContext.getPackageName());
     }
 }
