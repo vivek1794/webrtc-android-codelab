03fd2e789b6f3597e757a4089227fdfea5d49299
cat <nikhil.s.date@gmail.com>
2021-01-01 22:54:40 +0530

    removed unused classes

--- Step-3/app/src/main/java/xyz/vivekc/webrtccodelab/SignallingClient.java
+++ Step-3/app/src/main/java/xyz/vivekc/webrtccodelab/SignallingClient.java
@@ -71,7 +71,7 @@ class SignallingClient {
             IO.setDefaultHostnameVerifier((hostname, session) -> true);
             IO.setDefaultSSLContext(sslcontext);
             //set the socket.io url here
-            socket = IO.socket("https://stark-gorge-70808.herokuapp.com:443");
+            socket = IO.socket("<url>:<port>");
             socket.connect();
             Log.d("SignallingClient", "init() called");
 
